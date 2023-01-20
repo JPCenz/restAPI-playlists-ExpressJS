5c0c2f18d59dd88dcd3ee71c139bdc64c3884f33
JPCenz <p.cenzano5@gmail.com>
2023-01-20 11:21:22 -0500

    add: JWT authentication

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUser,signUp,login } from "./controller";
+import { getUser,signUp,login ,verifyToken} from "./controller";
 
 const userRouter: Router = Router();
 
 userRouter.get("/",getUser);
 userRouter.post("/",signUp);
 userRouter.post("/login",login);
+userRouter.post("/verify",verifyToken);
 
 export default userRouter;
--- src/components/user/utils/auth.ts
+++ src/components/user/utils/auth.ts
@@ -1,4 +1,5 @@
 import { compare, genSalt, hash } from "bcrypt";
+import jwt from "jsonwebtoken"
 
 export async function hashPassword(password: string): Promise<string> {
     const salt = await genSalt(12);
@@ -8,4 +9,23 @@ export async function hashPassword(password: string): Promise<string> {
 export async function comparePassword(plaintextPassword: string, hash:any) {
     const result = await compare(plaintextPassword, hash);
     return result;
+}
+
+export function signJwt(payload:Object,options={}) {
+    options ={
+        expiresIn :86400, // 24 hours
+    };
+    const secret : any  = process.env.TOKEN_SECRET;
+    return jwt.sign(payload,secret,options);
+}
+
+export function verifyJwt(token:string) {
+    try {
+        const secret : any  = process.env.TOKEN_SECRET;
+        return jwt.verify(token,secret)
+
+    } catch (error) {
+        return null
+    }
+    
 }
